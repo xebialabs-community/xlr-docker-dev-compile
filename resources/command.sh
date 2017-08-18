48e32096f670fcfaeebcc9a424fdba4900831587
Joris De Winne <joris.dewinne@gmail.com>
2017-08-18 11:38:51 -0700

    Making container compatible with gradle 4

--- resources/command.sh
+++ resources/command.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
-cd /data/
+mkdir /build
+cp -r /data/* /build
+
+cd /build/
 ./gradlew clean copyDownloadResources
 
 # Copy downloads into 'plugins'
 if [ -d "/data/build/downloads/plugins" ]; then
-    cp /data/build/downloads/plugins/* /opt/xlr/server/plugins
+    cp /build/build/downloads/plugins/* /opt/xlr/server/plugins
 fi
 
 # Compile plugin
-cd /data/
+cd /build/
 ./gradlew test assemble -PxlReleaseHome=/opt/xlr/server
+cp -rf /build/build/. /data/build
