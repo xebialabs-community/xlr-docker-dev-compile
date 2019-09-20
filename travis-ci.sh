7cbe6757ccf1b62858ce9622a2c6895eef2bed0e
Rick Broker <rbroker@blackpugsoftware.com>
2019-09-20 06:51:44 -0400

    add docker login

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "
+{
+\"username\": \"$DOCKER_USERNAME\",
+\"password\": \"$DOCKER_PASSWORD\"
+}
+" https://hub.docker.com/v2/users/login/ | jq -r .token)
+
 http --follow https://gist.githubusercontent.com/jdewinne/3f13494858fad8b6b2b88ebd3439f1ea/raw/2ca20118482b7790fdbc0a1e3cbe6a3d4e78933b/dockertags > ./dockertags
 chmod +x dockertags
 
@@ -9,5 +17,9 @@ image_name="xebialabsunsupported/xlr_dev_compile"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 while read tag ; do
     docker build -t $image_name:$tag --build-arg xlr_tag=$tag .
+    echo "####################################################################"
+    echo "####        $image_name:$tag built"
     docker push $image_name:$tag
+    echo "####        $image_name:$tag pushed"
+    echo "####################################################################"
 done < <(comm -23 <(sort /tmp/xlr) <(sort /tmp/xl))
